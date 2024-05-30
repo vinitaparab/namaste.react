f9e775e3ca21516505947e5b67640e13beb494ad
vinita <vinitaparab77@gmail.com>
2024-05-30 08:20:01 -0500

    EP-01 Ready to push

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-const parent=React.createElement("div",{id:"parent"},[
-        React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag"),
-]),
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
 
-        React.createElement("div",{id:"child2"},[
-                React.createElement("h1",{},"I am h1 tag"),
-                React.createElement("h2",{},"I am h2 tag"),
-        ]),
-]);    
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
 
-        console.log(parent);//object 
-        const root= ReactDOM.createRoot(document.getElementById("root"));
-    
-        root.render(parent);
+console.log(parent); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
